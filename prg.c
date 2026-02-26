3a3e0bca6d64684867e4aaa6ef48fac63c9f895c
Darshan <darshanjanud4@gmail.com>
2026-02-26 23:30:05 +0530

    finding the 2nd large num

---
+++ prg.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main()
+{
+    int a[10];
+    int large = INT_MIN;
+    int second = INT_MIN;
+    int i , n;
+    
+    printf("Enter the number of the array:");
+    scanf("%i",&n);
+    
+    if(n < 2)
+       return 0;
+      
+ printf("Enter the array elements:");
+    for( i=0 ; i<n; i++){
+       scanf("%i" , &a[i]);
+       
+              if(a[i] > large)
+            {
+               second = large;
+               large = a[i];
+             }
+
+             else if(a[i] < large && a[i] > second)
+            {
+               second = a[i];
+            }
+}
+          if(second == INT_MIN)
+           {
+            printf("ERROR");
+           
+           }
+            else
+            {
+            printf("the Second large element is :%i" , second);
+               
+            }
+     
+return 0;
+}
